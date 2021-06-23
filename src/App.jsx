33d88f225a22fba907ef1f2a50f7513857834dd2
Rahul Gautam Singh <rahul_ug@cse.nits.ac.in>
2021-06-23 14:20:49 +0545

    today

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,19 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
- 
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import Profile from "./Profile";
 import Login from "./Login";
 // import Planner from "./Planner";
-import Main from "./mainpage"
-
+import Main from "./mainpage";
 
 export default function App() {
   return (
     <Router>
-        <Switch>
-          <Route exact path="/">
-            <Login />
-          </Route>
-          <Route exact path="/main" component={Main} />
-         
-        
-        </Switch>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route exact path="/main" component={Main} />
+      </Switch>
     </Router>
   );
 }
